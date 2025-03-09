76b98d08a726362abdaa9ea597da0de38914dd72
Chiemerie Ezeoke <chiemerie@Chiemeries-MacBook-Air.local>
2025-03-09 20:08:48 +0000

    Fixed table navigation on the admin dashboard

--- apps/admin/src/app/shared/table-pagination.tsx
+++ apps/admin/src/app/shared/table-pagination.tsx
@@ -1,5 +1,5 @@
 import {Card, Nav, Pagination} from "@themesberg/react-bootstrap";
-import {Dispatch, SetStateAction, useCallback} from "react";
+import { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react';
 import {PaginationOptions} from "@coinvant/types";
 
 interface IProps {
@@ -10,8 +10,10 @@ interface IProps {
 }
 
 export const TablePagination = (props: IProps) => {
+  const [totalPages, setTotalPages] = useState(1);
+
 	const isExhausted = useCallback(() => {
-		if (props.items <= props.options.limit) {
+		if ((props.options.limit * props.options.page) >= props.total) {
 			return true;
 		}
 		return props.options.page * props.options.limit === props.total;
@@ -35,14 +37,32 @@ export const TablePagination = (props: IProps) => {
 		}
 	}
 
+  const navigateToPage = (page: number) => {
+    return () => {
+      props.setOptions(prevState => ({
+        ...prevState,
+        page,
+      }));
+    }
+  }
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(props.total / props.options.limit));
+  }, [props.total, props.options]);
+
 	return (
 		<Card.Footer className="px-3 border-0 d-lg-flex align-items-center justify-content-between">
 			<Nav>
-				<Pagination className="mb-2 mb-lg-0">
+				<Pagination className="mb-2 mb-lg-0 gap-2">
 					<Pagination.Prev disabled={props.options.page === 1} onClick={onPrevClick}>
 						Previous
 					</Pagination.Prev>
-					<Pagination.Item active>1</Pagination.Item>
+          {Array.from(Array(totalPages)).map((_, i) => (
+            <Pagination.Item key={i} onClick={navigateToPage(i + 1)}
+                             active={props.options.page === i + 1}>
+              {i + 1}
+            </Pagination.Item>
+          ))}
 					<Pagination.Next disabled={isExhausted()} onClick={onNextClick}>
 						Next
 					</Pagination.Next>
